72ca0358ea415428f0742b00ba69145364cb6ea2
Belenn333 <kiaraferh@gmail.com>
2024-05-07 00:00:44 -0500

    finalizacion de la implementacion del login y registrar

--- src/main/java/DAWI/ProyectoTiendaD/TODO/controller/HomeController.java
+++ src/main/java/DAWI/ProyectoTiendaD/TODO/controller/HomeController.java
@@ -1,2 +1,13 @@
-package DAWI.ProyectoTiendaD.TODO.controller;public class HomeController {
+package DAWI.ProyectoTiendaD.TODO.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(Model model){
+        return "home";
+    }
 }
